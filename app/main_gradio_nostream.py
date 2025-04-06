0ad5b5112537be54ac958754e94997530b9248bf
IG <panarin2005@yandex.ru>
2025-04-06 21:46:43 +0300

    error openai

--- app/main_gradio_nostream.py
+++ app/main_gradio_nostream.py
@@ -103,7 +103,7 @@ def generate_lesson_plan_interface(
         tools=tools if tools else None,
         tool_choice=tool_choice,
         max_output_tokens=2000,
-        stream=True
+        stream=False
     )
 ####### БЕЗ СТРИМИНГА
     return response.output_text
