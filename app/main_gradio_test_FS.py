69af16f04fec56c1b50adaced2e0e818b48333d7
PanarinI <84140021+PanarinI@users.noreply.github.com>
2025-04-10 21:50:13 +0300

    3s uploaded

--- app/main_gradio_test_FS.py
+++ app/main_gradio_test_FS.py
@@ -383,4 +383,4 @@ with gr.Blocks(theme=theme, css_paths=css_path) as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(share=True)
+    demo.launch()
--- test_responses.py
+++ test_responses.py
@@ -10,15 +10,14 @@ client = openai.AsyncOpenAI(
         timeout=60  # Устанавливаем время ожидания в 60 секунд
     )
 
-    # Отправляем запрос с инструментом web_search_preview
-    response = await client.responses.create(
-        model="gpt-4o-mini",  # Используем модель с поддержкой web_search
-        tools=[{"type": "web_search_preview"}],  # Указываем инструмент поиска
-        input="What was a positive news story from 1878? Find a different answer each request"  # Ваш запрос
-    )
+# Отправляем запрос с инструментом web_search_preview
+response = client.responses.create(
+    model="gpt-4o-mini",  # Используем модель с поддержкой web_search
+    tools=[{"type": "web_search_preview"}],  # Указываем инструмент поиска
+    input="What was a positive news story from 1878? Find a different answer each request"  # Ваш запрос
+)
 
-    # Получаем результат
-    output_text = response.output_text
-    print("Ответ:", output_text)
+# Получаем результат
+output_text = response.output_text
+print("Ответ:", output_text)
 
-asyncio.run(main())
