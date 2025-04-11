e9e02d2aa4ced4776be8340466e9bd87d5643126
IG <panarin2005@yandex.ru>
2025-04-11 15:27:25 +0300

    ...

--- app/main_gradio_test_FS.py
+++ app/main_gradio_test_FS.py
@@ -149,7 +149,7 @@ def generate_lesson_plan_interface(
        #tool_choice=tool_choice,
         include=["file_search_call.results"],
         max_output_tokens=2000,
-        temperature=1.5,
+        temperature=1,
         stream=False
     )
 
