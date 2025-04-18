80d9de588d524ad4e6787c120d43988651f60aad
IG <panarin2005@yandex.ru>
2025-04-18 17:17:27 +0300

    ...

--- test_post.py
+++ test_post.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://script.google.com/macros/s/ТВОЙ_КОД/exec"  # замени на свой
+url = "https://script.google.com/macros/s/AKfycbxEEWarPE7e8gcR4uVA83ciWdrFfNfro9VZkyW3x3Yw_e2yJfKEGg0GA6YeuO2FPAN2/exec"  # замени на свой
 data = {
     "comment": "тестовый отзыв",
     "rate": 5
