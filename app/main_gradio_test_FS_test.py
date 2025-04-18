3e0d2c234f6af51980c9ff87aeb665f32aaad9ca
IG <panarin2005@yandex.ru>
2025-04-18 15:42:33 +0300

    ...

--- app/main_gradio_test_FS_test.py
+++ app/main_gradio_test_FS_test.py
@@ -431,7 +431,10 @@ with gr.Blocks(theme=theme, css_paths=css_path) as demo:
 
                 send_feedback = gr.Button("📩 Отправить отзыв")
 
-                feedback_confirmation = gr.Markdown(visible=False)
+                feedback_confirmation = gr.Markdown(
+                    visible=False,
+                    elem_classes=["feedback-confirmation"]
+                )
 
                 gr.Markdown(
                     """
